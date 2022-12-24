a5102081e81e7a6901a96873b5e30cc654944f71
AnanthDhinakaran <ananthsibu00@gmail.com>
2022-12-24 10:33:13 +0530

    updated

--- UseMathematics.java
+++ UseMathematics.java
@@ -7,11 +7,11 @@ public class UseMathematics {
 		Mathematics div = (a,b)->a/b;
 		Mathematics mul = (b,i)->b*i;
 		Mathematics mod = (r,j)->r%j;
-		System.out.println(add.math(2, 5));
-		System.out.println(sub.math(2, 5));
-		System.out.println(mul.math(2, 5));
-		System.out.println(div.math(2, 5));
-		System.out.println(mod.math(2, 5));
+		System.out.println(add.math(5, 5));
+		System.out.println(sub.math(5, 7));
+		System.out.println(mul.math(5, 9));
+		System.out.println(div.math(6, 11));
+		System.out.println(mod.math(9, 34));
 		sub.print();
 	}
 
